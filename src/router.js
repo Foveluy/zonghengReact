65fefdddd7f2541494731a2e9eb2f8f9f32a498d
Z F <215566435@qq.com>
2018-04-09 17:21:52 +1200

    添加首页

--- src/router.js
+++ src/router.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { ProductList } from './page/product-list'
 import { ProductDetail } from './page/product-detail'
-
+import { Index } from './page/index'
 
 export default () => {
   return (
     <Router>
       <div>
-        <Route exact path="/list" component={ProductList} />
+        <Route exact path="/" component={Index} />
+        <Route path="/list" component={ProductList} />
         <Route path="/detail" component={ProductDetail} />
       </div>
     </Router>
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 export const MONEY_COLOR = '#ff7a45'
+export const FontSize = {
+  small: 12,
+  default: 15,
+  large: 18
+}
