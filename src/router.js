929a62e996fb7f7070f66743580fb91deb023904
方正 <215566435@qq.com>
2018-04-14 21:56:35 +1200

    测试一下ci

--- src/router.js
+++ src/router.js
@@ -2,17 +2,22 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { GlobalSpiner } from './component/spinner'
 
+const IMTEST = () => {
+    return <div>我是超级测试</div>
+}
+
 export default component => {
-  console.log('conponent', component)
-  return (
-    <Router>
-      <div>
-        <Route path="/course/:ticket" component={component['course']} />
-        <Route path="/trainerbooking/:id" component={component['trainer-booking']} />
-        <Route path="/list" component={component['product-list']} />
-        <Route path="/detail" component={component['product-detail']} />
-        <Route path="/recharge" component={component['recharge']} />
-      </div>
-    </Router>
-  )
+    console.log('conponent', component)
+    return (
+        <Router>
+            <div>
+                <Route exact path="/" component={IMTEST} />
+                <Route path="/course/:ticket" component={component['course']} />
+                <Route path="/trainerbooking/:id" component={component['trainer-booking']} />
+                <Route path="/list" component={component['product-list']} />
+                <Route path="/detail" component={component['product-detail']} />
+                <Route path="/recharge" component={component['recharge']} />
+            </div>
+        </Router>
+    )
 }
