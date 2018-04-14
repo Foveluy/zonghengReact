c11b8c77525f5e520e29921517c308f4b1a3b957
方正 <215566435@qq.com>
2018-04-14 22:46:42 +1200

    test

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { GlobalSpiner } from './component/spinner'
 
 const IMTEST = () => {
-    return <div>我是超级测试</div>
+    return <div>测试一下容器服务</div>
 }
 
 export default component => {
